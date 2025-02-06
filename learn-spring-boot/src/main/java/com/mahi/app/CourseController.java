597e349ab09cc6004b767b8582fd9e2bbb2d7905
Mahender Reddy <pmahenderreddy@users.noreply.github.com>
2025-02-06 16:43:53 +0100

    Created a course entiry/bean, sent list of courses as json (implicity happened)
    com.fasterxml.jackson library is real hero here, json to bean n vice versa. which is part of starter-web project

--- learn-spring-boot/src/main/java/com/mahi/app/CourseController.java
+++ learn-spring-boot/src/main/java/com/mahi/app/CourseController.java
@@ -1,9 +1,14 @@
 package com.mahi.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mahi.app.entities.Course;
 
 @RestController
 public class CourseController {
@@ -24,6 +29,15 @@ public class CourseController {
 	public String course(@PathVariable(value = "id", required = false) String id) {
 		String pageId = (id == null) ? "Welcome-Page" : id;
 		return "<h1>Course Page: " + pageId + "</h1>";
+	}
+	
+	@GetMapping("/course/all")
+	public List<Course> courses() {
+		return Arrays.asList(
+				new Course("1", "Learn Java", "Srisailam K"),
+				new Course("2", "Learn Spring", "Rajkumar P"),
+				new Course("3", "Learn Security", "Pavan S")
+				);
 	}
 
 }
