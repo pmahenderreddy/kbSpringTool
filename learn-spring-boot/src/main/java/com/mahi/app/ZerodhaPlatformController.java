317981a49bffaa43f09c69c68ff677e3ce83f292
Mahender Reddy <pmahenderreddy@users.noreply.github.com>
2025-02-07 13:59:14 +0100

    Enabling/Disabiling logging at root level, packages, class and adding logger, n logging details
    New stuff
    logging.level.root=INFO
    logging.level.org.springframework=WARN
    logging.level.com.mahi.app.ZerodhaPlatformController=DEBUG
    
    Logging levels ... more detailed to least and off
    TRACE (more detailed)
    DEBUG
    INFO
    WARN
    ERROR
    FATAL (least(
    OFF (no logging)

--- learn-spring-boot/src/main/java/com/mahi/app/ZerodhaPlatformController.java
+++ learn-spring-boot/src/main/java/com/mahi/app/ZerodhaPlatformController.java
@@ -1,5 +1,7 @@
 package com.mahi.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,19 @@ import com.mahi.app.entities.ZerodhaPlatformConfiguration;
 
 @RestController
 public class ZerodhaPlatformController {
-	
+
 	@Autowired
 	private ZerodhaPlatformConfiguration configurator;
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(ZerodhaPlatformController.class);
+
 	@RequestMapping("/zerodha-platform-details")
 	public ZerodhaPlatformConfiguration getZerodhaPlatformConfiguration() {
+		if (configurator == null) {
+			logger.debug(ZerodhaPlatformConfiguration.class + " configurator is null.");
+		} else {
+			logger.info("Request received to fetch details... @info");
+		}
 		return configurator;
 	}
 }
