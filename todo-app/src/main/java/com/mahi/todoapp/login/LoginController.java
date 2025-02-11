75b1ff66229572d093d3d54f131e65ff199bd8bf
Mahender Reddy <pmahenderreddy@users.noreply.github.com>
2025-02-11 09:37:39 +0530

    Logging basics ... created a logger
    added sample info log using the logger

--- todo-app/src/main/java/com/mahi/todoapp/login/LoginController.java
+++ todo-app/src/main/java/com/mahi/todoapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.mahi.todoapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	
+	// default logger: LogBack from slf4j
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@RequestMapping("/login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
@@ -14,7 +19,8 @@ public class LoginController {
 			name = "user";
 		}
 		model.put("name", name); // to make data available to view
+		logger.info("Pushed into model: query param name='{}'", name);
 
-		return "login";
+		return "login"; // forward to login.jsp view
 	}
 }
