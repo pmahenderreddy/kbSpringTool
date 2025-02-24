e38a5c002595ce2e8182b18f646eb63fe7f7ef70
Mahender Reddy <pmahenderreddy@users.noreply.github.com>
2025-02-25 00:50:24 +0530

    data-rest is simply superb (default endpoints are created)
    Learnings
    dependency  ...
    <artifactId>spring-boot-starter-data-rest</artifactId>
    
    localhost:8080 ... showing 1st level endpoints
    eg:
    http://localhost:8080/userDetailses/1
    http://localhost:8080/userDetailses/2
    http://localhost:8080/userDetailses?page=0&sort=name,asc&size=2
    http://localhost:8080/userDetailses?page=0&sort=name,desc&size=2
    
    in PostMan
    GET (list of all users)
    GET (get specified user details by id)
    POST (created new user)
    PUT (updated new user by id)
    DELETE (deleted a user by id)

--- first-rest-api/src/main/java/com/mahi/first_rest_api/user/UserDetailsCommandLineRunner.java
+++ first-rest-api/src/main/java/com/mahi/first_rest_api/user/UserDetailsCommandLineRunner.java
@@ -12,30 +12,27 @@ import org.springframework.stereotype.Component;
 public class UserDetailsCommandLineRunner implements CommandLineRunner {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+
 	private UserDetailsRepository repository;
-	
+
 	public UserDetailsCommandLineRunner(UserDetailsRepository repository) {
 		super();
 		this.repository = repository;
 	}
 
-
-
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info(Arrays.toString(args));
-		
+
 		repository.save(new UserDetails("Srisailam", "Admin"));
 		repository.save(new UserDetails("Mahender", "User"));
 		repository.save(new UserDetails("Ramakrishna", "User"));
-		
-		
+
 		List<UserDetails> users = repository.findAll();
 		users.forEach(user -> logger.info(user.toString()));
-		
-		logger.info(""+repository.count());
-		
+
+		logger.info("" + repository.count());
+
 		users = repository.findByRole("User");
 		users.forEach(user -> logger.info(user.toString()));
 
---
+++ first-rest-api/src/main/java/com/mahi/first_rest_api/user/UserDetailsRestRepository.java
@@ -0,0 +1,13 @@
+package com.mahi.first_rest_api.user;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserDetailsRestRepository
+		extends JpaRepository<UserDetails, Long>, PagingAndSortingRepository<UserDetails, Long> {
+	List<UserDetails> findByRole(String role);
+}
