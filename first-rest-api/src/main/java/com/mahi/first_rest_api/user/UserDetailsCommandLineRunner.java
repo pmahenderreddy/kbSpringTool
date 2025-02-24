a713ec697fd11f83c7af6e80228f504455474891
Mahender Reddy <pmahenderreddy@users.noreply.github.com>
2025-02-24 23:45:43 +0530

    Added data-jpa, data-rest and h2 database (to play around ... before data-rest integration)
    Learnings
    
    @Entity
    public class UserDetails { ... }
    
    public interface UserDetailsRepository extends JpaRepository<UserDetails, Long>
    
    @component
    public class UserDetailsCommandLineRunner implements CommandLineRunner

---
+++ first-rest-api/src/main/java/com/mahi/first_rest_api/user/UserDetailsCommandLineRunner.java
@@ -0,0 +1,43 @@
+package com.mahi.first_rest_api.user;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsCommandLineRunner implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private UserDetailsRepository repository;
+	
+	public UserDetailsCommandLineRunner(UserDetailsRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info(Arrays.toString(args));
+		
+		repository.save(new UserDetails("Srisailam", "Admin"));
+		repository.save(new UserDetails("Mahender", "User"));
+		repository.save(new UserDetails("Ramakrishna", "User"));
+		
+		
+		List<UserDetails> users = repository.findAll();
+		users.forEach(user -> logger.info(user.toString()));
+		
+		logger.info(""+repository.count());
+		
+		users = repository.findByRole("User");
+		users.forEach(user -> logger.info(user.toString()));
+
+	}
+}
