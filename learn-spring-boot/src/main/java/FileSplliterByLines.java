c6e050baaa400689490e5713eb848de770738861
Mahender Reddy <pmahenderreddy@users.noreply.github.com>
2025-03-05 00:05:23 +0530

    Creates new file runAll.bat
    Learnings
    To run all bat files individually is little error prone, might run same file twice or miss a file to run at all.
    this is removed all the above file issues

--- learn-spring-boot/src/main/java/FileSplliterByLines.java
+++ learn-spring-boot/src/main/java/FileSplliterByLines.java
@@ -7,19 +7,21 @@ public class FileSplliterByLines {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		FileSplliterByLines.splitFileByLines(
-				"D:\\myGitHub\\kbSpringTool\\learn-spring-boot\\src\\main\\resources\\split-staging\\source.txt", 5);
+				"D:\\myGitHub\\learn-spring-boot\\src\\main\\resources\\split-staging\\download.bat", 10);
 	}
 
 	public static void splitFileByLines(String sourceFilePath, int nrLines) throws Exception {
 		File sourceFile = new File(sourceFilePath);
 		Scanner inFile = new Scanner(sourceFile);
-		boolean isNext = false;
 		byte[] nlBytes = "\n".getBytes();
 		int fileNr = 0;
+		String runAllStr = "";
 		while (inFile.hasNext()) {
-			File newFile = new File(sourceFile.getParent() + "/file-part-" + (++fileNr) + ".bat");
+			String newFileName = "file-part-" + (++fileNr) + ".bat";
+			runAllStr += "start \"\" "+newFileName + "\n";
+			File newFile = new File(sourceFile.getParent() + "/" + newFileName);
 			FileOutputStream outFile = new FileOutputStream(newFile, true);
-			System.out.println(fileNr+" file created "+newFile.getName());
+			System.out.println(fileNr + " file created " + newFile.getName());
 
 			for (int i = 0; i < nrLines; i++) {
 				if (inFile.hasNext()) {
@@ -31,6 +33,9 @@ public class FileSplliterByLines {
 			}
 			outFile.close();
 		}
+		File newFile = new File(sourceFile.getParent() + "/runAll.bat");
+		FileOutputStream outFile = new FileOutputStream(newFile, true);
+		outFile.write(runAllStr.getBytes());
+		outFile.close();
 	}
-
 }
